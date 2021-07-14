3ee016f23a96bf6ddda0ba5d2d08fe9fe2e8ac50
ErickPxndx <erickpxndx112@gmail.com>
2021-07-14 00:11:24 -0500

    Homework realizada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -257,6 +257,16 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí:
+
+  var tabla = [];
+
+  for(let i = 0; i >= 10; i++)
+  {
+    var multi = i * 6;
+    tabla.push(multi);
+  }
+
+  return tabla;
   
 }
 
@@ -275,6 +285,17 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+
+  do
+  {
+    var i = 1;
+    numero = i + 5;
+    i++;
+  }
+  
+  while(i >= 8);
+
+  return numero;
 
 }
 
